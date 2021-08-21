70cfc2d275160d03d081fcd043cf285888176ffc
= <86680816+ehlui@users.noreply.github.com>
2021-08-21 03:31:29 +0200

    Adding html paginations with no logic (buttons are not working yet)

--- Docker-API-lotr/services/api/tolkien/static/js/calls.js
+++ Docker-API-lotr/services/api/tolkien/static/js/calls.js
@@ -2,12 +2,18 @@ const api = 'tolkien'
 const endpoints = {
     'books': `/${api}/books`,
     'movies': `/${api}/movies`,
-    'chapters': `/${api}/chapters`
+    'chapters': `/${api}/chapters`,
 }
-
+const endpoints_with_paginations = {
+    'chap_test': `/${api}/chap_test`
+}
 
 for (const [k, v] of Object.entries(endpoints)) {
     fetch_api(v, k, k);
+}
+
+for (const [k, v] of Object.entries(endpoints_with_paginations)) {
+    fetch_api_paginations(v, k, k);
 }
 
 function addstyle() {
--- Docker-API-lotr/services/api/tolkien/static/js/retrieve.js
+++ Docker-API-lotr/services/api/tolkien/static/js/retrieve.js
@@ -66,6 +66,68 @@ function fetch_api(endpoint, data_name, tag_id) {
             make_table_from_array(result[data_name], tag_id, data_name);
         })
         .catch(error => { console.error('Error:', error); return error; });
+}
 
+function fetch_api_paginations(endpoint, data_name, tag_id) {
+    var data_container = document.getElementById(tag_id)
+    var promise = fetch(endpoint)
+        .then(response => { return response.json() })
 
+    if (data_container == null) {
+        create_html("div", tag_id, "table-container")
+        var data_container = document.getElementById(tag_id)
+    }
+
+    data_container.innerHTML = "<p>Loading...</p>"
+
+    promise
+        .then(result => {
+            manage_paginations(result, tag_id, data_name);
+        })
+        .catch(error => { console.error('Error:', error); return error; });
+}
+
+function manage_paginations(result, tag_id, data_name) {
+    const results = Object.values(result["result"])
+    var data_container = document.getElementById(tag_id)
+    var prev_url = result["prev_url"]
+    var next_url = result["next_url"]
+    var table = "<table>"
+    var rows = "";
+
+    var title = document.createElement("h2");
+    var text_title = document.createTextNode(data_name);
+    title.setAttribute('id', `title-${tag_id}`);
+    title.appendChild(text_title);
+
+    for (const r of results) {
+        rows = rows.concat('<tr>')
+        for (const k of Object.keys(r)) {
+            rows = rows.concat(`<td>${r[k]}</td>`)
+        }
+        rows = rows.concat('</tr>')
+    }
+
+    table = table.concat(rows).concat("</table>")
+    data_container.innerHTML = table
+    data_container.prepend(title);
+
+    if (next_url) {
+        var btn_next = build_btn_pag("Next", "btn-next", next_url)
+        data_container.append(btn_next);
+    }
+    if (prev_url) {
+        var btn_prev = build_btn_pag("Previous", "btn-prev", prev_url)
+        data_container.append(btn_prev);
+    }
+
+}
+
+function build_btn_pag(text, id, href) {
+    var btn = document.createElement("button");
+    text_title = document.createTextNode(text);
+    btn.setAttribute('id', id);
+    btn.setAttribute('href', href);
+    btn.appendChild(text_title);
+    return btn;
 }
